8db429316f9f48486afa10b0cd1faf1335819d6a
Renhao Hu <renhaohu@gmail.com>
2021-06-14 21:32:25 -0700

    add base files

---
+++ app/src/main/java/com/renhao/cats/views/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.renhao.cats.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.renhao.cats.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
